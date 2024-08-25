b17f323b5f03792f1fadc62be8f120ad7d0fd872
E. Frank C. Lapa <frank.lapa@itexp.co.uk>
2024-08-25 18:07:19 +0100

    Add Java project

---
+++ src/main/java/com/caltech/devops/jenkins/caltech_jenkins_project_01/CaltechJenkinsProject01Application.java
@@ -0,0 +1,13 @@
+package com.caltech.devops.jenkins.caltech_jenkins_project_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaltechJenkinsProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaltechJenkinsProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/devops/jenkins/caltech_jenkins_project_01/CaltechJenkinsProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.devops.jenkins.caltech_jenkins_project_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaltechJenkinsProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
